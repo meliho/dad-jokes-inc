2c299f88d3dabc409a96ea4fa79cee063e046896
Melih Onvural <melih.onvural@gmail.com>
2021-07-12 12:37:13 -0400

    Initial HelloWorld commit

---
+++ src/main/java/com/dadjokesinc/DadJokesIncApplication.java
@@ -0,0 +1,22 @@
+package com.dadjokesinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DadJokesIncApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadJokesIncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dadjokesinc/DadJokesIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadjokesinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokesIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
